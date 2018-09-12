f26be6935e3292ce509ac94932c56f66efd98c51
liuyang <250935102@qq.com>
2018-09-12 09:47:08 +0800

    iuu杨

--- W2K/src/main/java/cc/liuy/Service/JsoupUtil.java
+++ W2K/src/main/java/cc/liuy/Service/JsoupUtil.java
@@ -6,20 +6,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-//http://www.open-open.com/jsoup/dom-navigation.htm
+
 public class JsoupUtil {
-    public  static void main(String[] args) {
-       try {
-           String  url ="https://mp.weixin.qq.com/s/WV3-13J-d5TDmirDDQ_VrQ";
-           Document doc = Jsoup.connect(url).get();
-           log(doc.title());
-           Elements newsHeadlines = doc.getElementsByAttributeValue("","")
-           for (Element headline : newsHeadlines) {
-               log(headline.text());
-           }
-       }catch (IOException e){
-           System.out.println("IOExcepiton");
-       }
+    public static void main(String[] args) {
+        try {
+            String url = "https://mp.weixin.qq.com/s/WV3-13J-d5TDmirDDQ_VrQ";
+            Document doc = Jsoup.connect(url).get();
+            log(doc.title());
+            Elements newsHeadlines = doc.getElementsByTag("span");
+            Elements src = doc.getElementsByTag("data-src");
+
+            for (Element headline : src) {
+                log(headline.text());
+            }
+            for (Element headline : newsHeadlines) {
+                log(headline.text());
+            }
+        } catch (IOException e) {
+            System.out.println("IOExcepiton");
+        }
 
     }
 
